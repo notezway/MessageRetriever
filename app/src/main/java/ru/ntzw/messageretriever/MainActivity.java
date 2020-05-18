473fa5a49d41f7e2b0ed25880569f9010f58578d
Vladislav Baykov <notezway@gmail.com>
2020-05-18 12:29:41 +0300

    Initial commit

---
+++ app/src/main/java/ru/ntzw/messageretriever/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.ntzw.messageretriever;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
