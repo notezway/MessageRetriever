95fa85220ad759221c5468690c2132e5e77eab5a
Vladislav Baykov <notezway@gmail.com>
2020-05-22 18:40:18 +0300

    Message id added Visual improvements

--- app/src/main/java/ru/ntzw/messageretriever/MessageViewHolder.java
+++ app/src/main/java/ru/ntzw/messageretriever/MessageViewHolder.java
@@ -8,13 +8,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 class MessageViewHolder extends RecyclerView.ViewHolder {
 
+    private final TextView idView;
     private final TextView timeView;
     private final TextView textView;
 
     MessageViewHolder(@NonNull View itemView) {
         super(itemView);
+        this.idView = itemView.findViewById(R.id.tv_id);
         this.timeView = itemView.findViewById(R.id.tv_time);
         this.textView = itemView.findViewById(R.id.tv_text);
+    }
+
+    TextView getIdView() {
+        return idView;
     }
 
     TextView getTimeView() {
